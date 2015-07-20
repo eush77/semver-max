a38e137388c149762be5625bd214b3ba0f637564
Eugene Sharygin <eush77@gmail.com>
2015-07-20 20:12:43 +0300

    Add other comparators

--- index.js
+++ index.js
@@ -3,14 +3,37 @@
 var semver = require('semver');
 
 
-var max2 = function (v1, v2) {
-  console.log(v1, 'vs', v2);
-  return semver.gt(v2, v1) ? v2 : v1;
+var makeMax2 = function (op) {
+  return function (v1, v2) {
+    return semver[op](v2.clean, v1.clean) ? v2 : v1;
+  };
 };
 
 
-module.exports = function () {
-  return []
-    .filter.call(arguments, semver.valid)
-    .reduce(max2);
+var makeMax = function (op) {
+  return function () {
+    return []
+      .filter.call(arguments, function (version) {
+        return semver.valid(version, true);
+      })
+      .map(function (version) {
+        return {
+          original: version,
+          clean: semver.clean(version)
+        };
+      })
+      .reduce(makeMax2(op))
+      .original;
+  };
 };
+
+
+module.exports = (function (max) {
+  max.gt = max;
+
+  ['gte', 'lt', 'lte'].forEach(function (op) {
+    max[op] = makeMax(op);
+  });
+
+  return max;
+}(makeMax('gt')));
--- test/test.js
+++ test/test.js
@@ -14,7 +14,60 @@ test(function (t) {
 });
 
 
+// > msg = msgBase("Base part")
+// > msg("suffix")
+// "Base part (suffix)"
+var msgBase = function (str) {
+  return function (suffix) {
+    return str + ' (' + suffix + ')';
+  };
+};
+
+
+test('comparators', function (t) {
+  t.equal(semverMax, semverMax.gt, 'semverMax === semverMax.gt');
+
+  (function (msg) {
+    ['gt', 'gte', 'lt', 'lte'].forEach(function (op) {
+      t.equal(semverMax[op]('=1.0.0'), '=1.0.0', msg(op));
+    });
+  }(msgBase('one argument')));
+
+  (function (msg) {
+    [{ op: 'gt', answer: '1.0.0' },
+     { op: 'gte', answer: '1.0.0' },
+     { op: 'lt', answer: '0.0.0' },
+     { op: 'lte', answer: '0.0.0' }].forEach(function (cc) {
+       t.equal(semverMax[cc.op]('0.0.0', '1.0.0'), cc.answer, msg(cc.op));
+     });
+  }(msgBase('two arguments')));
+
+  (function (msg) {
+    [{ op: 'gt', answer: '1.0.0' },
+     { op: 'gte', answer: 'v1.0.0' },
+     { op: 'lt', answer: '=0.0.0' },
+     { op: 'lte', answer: '0.0.0' }].forEach(function (cc) {
+       t.equal(semverMax[cc.op]('=0.0.0', '1.0.0', 'v0.1.0', 'v1.0.0', '0.0.0'),
+               cc.answer, msg(cc.op));
+     });
+  }(msgBase('many arguments')));
+
+  t.end();
+});
+
+
 test('use with reduce', function (t) {
   t.equal(['0.0.0', '0.1.0', '1.0.0'].reduce(semverMax), '1.0.0', 'semverMax');
+
+  var msgBase = 'semverMax.';
+
+  [{ op: 'gt', answer: '1.0.0' },
+   { op: 'gte', answer: 'v1.0.0' },
+   { op: 'lt', answer: '=0.0.0' },
+   { op: 'lte', answer: '0.0.0' }].forEach(function (cc) {
+     t.equal(['=0.0.0', '1.0.0', 'v0.1.0', 'v1.0.0', '0.0.0']
+             .reduce(semverMax[cc.op]), cc.answer, msgBase + cc.op);
+   });
+
   t.end();
 });
